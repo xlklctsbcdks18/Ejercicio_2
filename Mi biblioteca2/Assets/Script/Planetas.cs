47424be6cbf47cc29abf0c1bd5261447af56aa88
Celina Garcia <mcelina_21@hotmail.com>
2015-10-28 15:33:08 -0600

    Agregue esto

---
+++ Mi biblioteca2/Assets/Script/Planetas.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Planetas : MonoBehaviour {
+	public GameObject center;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.RotateAround(new Vector3(0,10,0), Vector3.up, speed * Time.deltaTime);
+	
+	}
+}
